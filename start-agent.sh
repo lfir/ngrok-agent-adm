caf9dd3f94120ff86da6f59b62b22a660abe903b
Leandro <psljp@protonmail.com>
2022-12-29 16:39:12 -0300

    Update start-agent.sh

--- start-agent.sh
+++ start-agent.sh
@@ -8,5 +8,5 @@ sleep 10
 
 url="{$(curl http://localhost:4040/api/tunnels | grep -oP '"public_url":"(.*?)"')}"
 echo "$url" > tunnel-url.json
-git commit -am "$(date +%d-%m-%yT%H:%M:%S)"
+git commit -am "Public URL update @ $(date +%d-%m-%yT%H:%M:%S)"
 git push
